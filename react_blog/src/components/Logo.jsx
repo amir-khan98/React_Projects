0ce515f33ab95bb40279ca37f6c20f1dc3aa29a5
Muhammad Amir <amirgurmani64@gmail.com>
2026-08-02 18:05:07 +0000

    react_blog

--- react_blog/src/components/Logo.jsx
+++ react_blog/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src="../../public/Logo.png" alt="" width={'65px'} />
+      <img src="/Logo.png" alt="Logo" width={'65px'} />
     </div>
   )
 }
